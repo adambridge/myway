bac1b44ff7084419adea8855f16528e665375e91
Adam Bridge <bridge.adam@gmail.com>
2020-09-29 23:49:11 +0100

    Added reset option

--- myway.sh
+++ myway.sh
@@ -1,7 +1,26 @@
-#!/bin/bash 
+#!/bin/bash
 MYWAY_TAG="MYWAY-AUTO-INSTALL"
 BASH_COMMENT="#"
 VIM_COMMENT="\""
+
+function reset_config() {
+    local TGT=$1
+    local TMP=$(mktemp /tmp/temp.XXXXXXXX)
+
+    grep -v $MYWAY_TAG $TGT > $TMP
+    case $? in
+    0)  # At least one line returned
+        cp $TMP $TGT && { echo $TGT restored. || echo Failed to restore $TGT; }
+        ;;
+    1)  # No lines but no error
+        rm $TGT && { echo $TGT removed. || echo Failed to remove $TGT; }
+        ;;
+    2)  # Error
+        echo Error restoring $TGT
+        ;;
+    esac
+    rm $TMP
+}
 
 function update_config() {
     local SRC=$1
@@ -48,6 +67,22 @@ function git_config() {
     fi
 }
 
+if [ ! -z $1 ]; then
+    if [ $1 == "reset" ]; then
+        reset_config ~/.vimrc
+        reset_config ~/.bash_aliases
+        reset_config ~/.selected_editor
+        git config --global --unset user.name
+        git config --global --unset user.email
+        sudo apt-get purge vim
+        sudo apt-get autoremove
+        exit 0
+    else
+        echo usage: ./myway.sh [reset]
+        exit 1
+    fi
+fi
+
 # Vim
 which vim &> /dev/null || sudo apt-get install vim
 update_config ./vimrc ~/.vimrc $VIM_COMMENT
@@ -59,6 +94,6 @@ update_config ./selected_editor ~/.selected_editor $BASH_COMMENT
 # Disable bell? update_config ./inputrc /etc/inputrc
 
 # Git
-git_config user.email "Enter git user.email: you@example.com:"
 git_config user.name "Enter git user.name: Your Name (no quotes):"
+git_config user.email "Enter git user.email: you@example.com:"
 
